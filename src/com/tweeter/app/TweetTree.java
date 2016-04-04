a3abd7db26ef1dda8f33dd94c1dbd0f7880d388c
Nick Walker <nwalker@syr.edu>
2016-04-04 16:48:28 -0400

    Made TweetTree, not a heap... Yet...

--- src/com/tweeter/app/TweetTree.java
+++ src/com/tweeter/app/TweetTree.java
@@ -1,9 +1,15 @@
 package com.tweeter.app;
 
-public class TweetTree {
+//TODO Actually make this a heap...
 
+public class TweetTree {
+	/**
+	 * "center" node of tweet tree
+	 */
+	private TweetNode root;
+	
 	public TweetTree() {
-		// TODO Auto-generated constructor stub
+		root = null;
 	}
 	
 	/**
@@ -14,7 +20,10 @@ public class TweetTree {
 	 * @param y : y coordinate of bird
 	 */
 	public void addTweet(Tweet tweet, int x, int y){
-		
+		if (root == null)
+			root = new TweetNode(tweet, x, y);
+		else {
+			root.addTweet( tweet, x, y ); }
 	}
 	
 	/**
@@ -29,7 +38,11 @@ public class TweetTree {
 	 * @return closest tweet to the listening bird
 	 */
 	public TweetNode listen(int x, int y){
-		return new TweetNode();
+		TweetNode closest = null;
+		
+		
+		
+		return closest;
 	}
 }
 
@@ -39,35 +52,82 @@ public class TweetTree {
  * @author nick
  */
 class TweetNode {
-	public int x;
-	public int y;
+	public int x0;
+	public int y0;
 	public Tweet tweet;
-	private TweetNode q1, q2, q3, q4;
+	protected TweetNode NE, NW, SE, SW;
 	
+	/**
+	 * Constructor of a node for Tweet quad tree.  
+	 * 
+	 * @param t : tweet to be put in tree
+	 * @param x : x coordinate of tweet
+	 * @param y : y coordinate of tweet
+	 */
+	public TweetNode(Tweet t, int x, int y){
+		x0 = x;
+		y0 = y;
+		tweet = t;
+		SE = null;
+		SW = null;
+		NE = null;
+		NW = null;
+	}
+	
+	public boolean addTweet(Tweet t, int x, int y){
+		if (x - x0 > 0){
+			// Q1
+			if (y - y0 > 0) {
+				if (SE == null)
+					SE = new TweetNode(t, x, y);
+				else
+					SE.addTweet(t, x, y); }
+			// Q3
+			else {
+				if (NE == null)
+					NE = new TweetNode(t, x, y);
+				else
+					NE.addTweet(t, x, y); }}
+		else {
+			// Q2
+			if (y - y0 > 0){
+				if (SW == null)
+					SE = new TweetNode(t, x, y);
+				else
+					SW.addTweet(t, x, y); }
+			// Q4
+			else {
+				if (NW == null)
+					NW = new TweetNode(t, x, y);
+				else
+					NW.addTweet(t, x, y); }
+		}
+			
+		return true;
+	}
 	
 	//Getters and setters
-	public TweetNode getQ1() {
-		return q1;
-	}
-	public void setQ1(TweetNode q1) {
-		this.q1 = q1;
-	}
-	public TweetNode getQ2() {
-		return q2;
-	}
-	public void setQ2(TweetNode q2) {
-		this.q2 = q2;
-	}
-	public TweetNode getQ3() {
-		return q3;
-	}
-	public void setQ3(TweetNode q3) {
-		this.q3 = q3;
-	}
-	public TweetNode getQ4() {
-		return q4;
-	}
-	public void setQ4(TweetNode q4) {
-		this.q4 = q4;
-	}
+	public TweetNode getNE() {
+		return NE;}
+
+	public void setNE(TweetNode nE) {
+		NE = nE;}
+
+	public TweetNode getNW() {
+		return NW;}
+
+	public void setNW(TweetNode nW) {
+		NW = nW;}
+
+	public TweetNode getSE() {
+		return SE;}
+
+	public void setSE(TweetNode sE) {
+		SE = sE;}
+
+	public TweetNode getSW() {
+		return SW;}
+
+	public void setSW(TweetNode sW) {
+		SW = sW;}
 }
